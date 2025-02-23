835948bcac7e5e3bf11ea7a3f2339bcf8a87ad15
igor <igor.jimenez@sptech.school>
2025-02-23 02:49:27 -0300

    build: Criação das dependencias e estrutura do projeto

---
+++ src/test/java/tech/vitalis/caringu/CaringuApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.vitalis.caringu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaringuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
