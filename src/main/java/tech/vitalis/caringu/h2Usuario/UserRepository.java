e89ce6a5b475c6f072a6b4de639c7b48ca3ec843
Gustavo Kohatsu <161063190+Gustavo-Kohatsu@users.noreply.github.com>
2025-02-27 00:12:21 -0300

    Revert "Feature/crud usuario"

--- src/main/java/tech/vitalis/caringu/h2Usuario/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package tech.vitalis.caringu.h2Usuario;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<Usuario, Integer> {
-
-}
