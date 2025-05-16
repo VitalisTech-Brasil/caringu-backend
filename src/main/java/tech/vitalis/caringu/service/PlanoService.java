b8bcce101ad40dc9f299eab8e05f6aa8d7b866fa
Gustavo Kohatsu <gustavo.kohatsu@sptech.school>
2025-05-16 20:27:08 -0300

    fix: corrigindo problemas de merge + removendo imports sem utilizacao

--- src/main/java/tech/vitalis/caringu/service/PlanoService.java
+++ src/main/java/tech/vitalis/caringu/service/PlanoService.java
@@ -1,39 +1,25 @@
 package tech.vitalis.caringu.service;
 
 import jakarta.persistence.EntityNotFoundException;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.transaction.Transactional;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 import org.springframework.stereotype.Service;
-import tech.vitalis.caringu.dtos.Aluno.AlunoResponseGetDTO;
 import tech.vitalis.caringu.dtos.PersonalTrainer.PersonalTrainerResponseGetDTO;
 import tech.vitalis.caringu.dtos.Plano.PlanoRequisicaoRecord;
 import tech.vitalis.caringu.dtos.Plano.PlanoRespostaRecord;
 import tech.vitalis.caringu.dtos.PlanoContratado.PlanoContratadoRespostaRecord;
-import tech.vitalis.caringu.dtos.Treino.TreinoRequestUpdateDto;
-import tech.vitalis.caringu.dtos.Treino.TreinoResponseGetDTO;
-
 import tech.vitalis.caringu.entity.*;
 import tech.vitalis.caringu.enums.PeriodoEnum;
 import tech.vitalis.caringu.enums.StatusEnum;
 import tech.vitalis.caringu.exception.ApiExceptions;
-import tech.vitalis.caringu.mapper.AlunoMapper;
-
 import tech.vitalis.caringu.mapper.PersonalTrainerMapper;
 import tech.vitalis.caringu.mapper.PlanoContratadoMapper;
 import tech.vitalis.caringu.mapper.PlanoMapper;
-
 import tech.vitalis.caringu.repository.AlunoRepository;
 import tech.vitalis.caringu.repository.PlanoContratadoRepository;
-
 import tech.vitalis.caringu.repository.PlanoRepository;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class PlanoService {
@@ -45,31 +31,20 @@ public class PlanoService {
 
     private final PersonalTrainerService personalTrainerService;
     private final PersonalTrainerMapper personalTrainerMapper;
-    private final PersonalTrainerRepository personalTrainerRepository;
 
-    private final AlunoService alunoService;
-    private final AlunoMapper alunoMapper;
     private final AlunoRepository alunoRepository;
 
-    public PlanoService(PlanoRepository planoRepository, PlanoMapper planoMapper, PlanoContratadoRepository planoContratadoRepository, PlanoContratadoMapper planoContratadoMapper, PersonalTrainerService personalTrainerService, PersonalTrainerMapper personalTrainerMapper, AlunoService alunoService, AlunoMapper alunoMapper, AlunoRepository alunoRepository) {
+    public PlanoService(PlanoRepository planoRepository, PlanoMapper planoMapper, PlanoContratadoRepository planoContratadoRepository, PlanoContratadoMapper planoContratadoMapper, PersonalTrainerService personalTrainerService, PersonalTrainerMapper personalTrainerMapper, AlunoRepository alunoRepository) {
         this.planoRepository = planoRepository;
         this.planoMapper = planoMapper;
         this.planoContratadoRepository = planoContratadoRepository;
         this.planoContratadoMapper = planoContratadoMapper;
         this.personalTrainerService = personalTrainerService;
         this.personalTrainerMapper = personalTrainerMapper;
-        this.alunoService = alunoService;
-        this.alunoMapper = alunoMapper;
         this.alunoRepository = alunoRepository;
     }
 
     public List<PlanoRespostaRecord> listarPlanosPorPersonal(Integer personalId) {
-        boolean personalTrainer = personalTrainerRepository.existsById(personalId);
-
-        if (!personalTrainer) {
-            throw new PersonalNaoEncontradoException("Personal Trainer não encontrado");
-        }
-
         List<Plano> listaDePlanos = planoRepository.findByPersonalTrainerId(personalId);
         return planoMapper.PlanoListToResponseList(listaDePlanos);
     }
