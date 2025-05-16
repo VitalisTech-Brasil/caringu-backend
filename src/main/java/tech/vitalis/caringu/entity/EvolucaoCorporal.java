4251a79011e202b8f203c346319138348db4c82f
Gustavo Kohatsu <gustavo.kohatsu@sptech.school>
2025-05-16 20:35:44 -0300

    fix: removendo mappers do tipo interface para interromper bug de ter que utilizar mvn clean install toda hora + entidade evolucao_corporal

--- src/main/java/tech/vitalis/caringu/entity/EvolucaoCorporal.java
+++ src/main/java/tech/vitalis/caringu/entity/EvolucaoCorporal.java
@@ -1,34 +1,34 @@
 package tech.vitalis.caringu.entity;
-
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
-import lombok.*;
 import tech.vitalis.caringu.enums.TipoEvolucaoEnum;
-
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "evolucao_corporal")
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class EvolucaoCorporal {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     @NotNull
     @Enumerated(EnumType.STRING)
     private TipoEvolucaoEnum tipo;
+
+    @Column(name = "url_foto_shape", columnDefinition = "TEXT")
     private String urlFotoShape;
+
     @NotNull
+    @Column(name = "data_envio")
     private LocalDateTime dataEnvio;
+
     @Positive
     @NotNull
+    @Column(name = "periodo_avaliacao")
     private Integer periodoAvaliacao;
+
     @NotNull
     @ManyToOne
     @JoinColumn(name = "alunos_id")
--- src/main/java/tech/vitalis/caringu/mapper/EstadoMapper.java
+++
@@ -1,7 +0,0 @@
-package tech.vitalis.caringu.mapper;
-
-import org.mapstruct.Mapper;
-
-@Mapper(componentModel = "spring")
-public interface EstadoMapper {
-}
