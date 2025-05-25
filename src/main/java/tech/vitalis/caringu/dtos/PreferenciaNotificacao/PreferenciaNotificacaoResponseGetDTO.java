e496d48f313f34ab6048d86e94266a8184298c6e
Gustavo Kohatsu <gustavo.kohatsu@sptech.school>
2025-05-25 03:46:34 -0300

    fix: adicionando campo id nas preferencias notificacao, para front receber com identificacao

--- src/main/java/tech/vitalis/caringu/dtos/PreferenciaNotificacao/PreferenciaNotificacaoResponseGetDTO.java
+++ src/main/java/tech/vitalis/caringu/dtos/PreferenciaNotificacao/PreferenciaNotificacaoResponseGetDTO.java
@@ -3,6 +3,7 @@ package tech.vitalis.caringu.dtos.PreferenciaNotificacao;
 import tech.vitalis.caringu.enums.PreferenciaNotificacao.TipoPreferenciaEnum;
 
 public record PreferenciaNotificacaoResponseGetDTO(
+        Integer id,
         TipoPreferenciaEnum tipo,
         boolean ativada
 ) {
