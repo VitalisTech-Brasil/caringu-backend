e1515beb080b77a779e17f5d5bf4b81e6e9ec927
Gustavo-Kohatsu <gustavo.kohatsu@sptech.school>
2025-05-28 19:43:01 -0300

    feat: adicionando regra de negocio para preencher data fim e contratacao se for para ativar o plano contratado

--- src/main/java/tech/vitalis/caringu/service/PlanoContratadoService.java
+++ src/main/java/tech/vitalis/caringu/service/PlanoContratadoService.java
@@ -4,11 +4,13 @@ import org.springframework.stereotype.Service;
 import tech.vitalis.caringu.dtos.Aluno.PlanoPertoFimResponseDTO;
 import tech.vitalis.caringu.dtos.PlanoContratado.PlanoContratadoPendenteRequestDTO;
 import tech.vitalis.caringu.entity.PlanoContratado;
+import tech.vitalis.caringu.enums.PeriodoEnum;
 import tech.vitalis.caringu.enums.StatusEnum;
 import tech.vitalis.caringu.exception.PlanoContratado.PlanoContratadoNaoEncontradoException;
 import tech.vitalis.caringu.repository.PlanoContratadoRepository;
 import tech.vitalis.caringu.strategy.PlanoContratado.StatusEnumValidationStrategy;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
 
@@ -28,14 +30,31 @@ public class PlanoContratadoService {
     }
 
     public void atualizarStatus(Integer id, StatusEnum novoStatus) {
-        PlanoContratado plano = planoContratadoRepository.findById(id)
+        PlanoContratado planoContratado = planoContratadoRepository.findById(id)
                 .orElseThrow(() -> new PlanoContratadoNaoEncontradoException("Plano contratado com id %d não encontrado.".formatted(id)));
 
         validarEnums(Map.of(
                 new StatusEnumValidationStrategy(), novoStatus
         ));
 
-        plano.setStatus(novoStatus);
-        planoContratadoRepository.save(plano);
+        boolean isAvulso = planoContratado.getPlano().getPeriodo().equals(PeriodoEnum.AVULSO);
+        boolean isMensal = planoContratado.getPlano().getPeriodo().equals(PeriodoEnum.MENSAL);
+        boolean isSemestral = planoContratado.getPlano().getPeriodo().equals(PeriodoEnum.SEMESTRAL);
+
+        if (novoStatus.equals(StatusEnum.ATIVO)) {
+            if (!isAvulso) {
+                planoContratado.setDataContratacao(LocalDate.now());
+
+                if (isMensal) {
+                    planoContratado.setDataFim(LocalDate.now().plusMonths(1));
+
+                } else if (isSemestral) {
+                    planoContratado.setDataFim(LocalDate.now().plusMonths(6));
+                }
+            }
+        }
+
+        planoContratado.setStatus(novoStatus);
+        planoContratadoRepository.save(planoContratado);
     }
 }
