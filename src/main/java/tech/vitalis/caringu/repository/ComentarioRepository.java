d3ca19cbe0741024443ab0ec247c5f57e2a7cc06
Gustavo Kohatsu <gustavo.kohatsu@sptech.school>
2025-09-20 18:05:31 -0300

    fix: excluindo entidade Comentario que não será utilizada

--- src/main/java/tech/vitalis/caringu/repository/ComentarioRepository.java
+++
@@ -1,9 +0,0 @@
-package tech.vitalis.caringu.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import tech.vitalis.caringu.entity.Comentario;
-
-@Repository
-public interface ComentarioRepository extends JpaRepository<Comentario, Integer> {
-}
