9ab8ec2a4a0618c114a516426f6145a0bf0d0847
biasouza21 <bianca.bs2211@gmail.com>
2025-09-25 18:52:38 -0300

    feat: entidade Avaliacao e recursos, endpoint de listar por personal e de cadastro

---
+++ src/main/java/tech/vitalis/caringu/controller/AvaliacaoController.java
@@ -0,0 +1,28 @@
+package tech.vitalis.caringu.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import tech.vitalis.caringu.dtos.Avaliacao.AvaliacaoRequestDTO;
+import tech.vitalis.caringu.dtos.Avaliacao.AvaliacaoResponseDTO;
+import tech.vitalis.caringu.service.AvaliacaoService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/avaliacoes")
+public class AvaliacaoController {
+    @Autowired
+    private AvaliacaoService avaliacaoService;
+
+    @GetMapping("/personal/{idPersonal}")
+    public ResponseEntity<List<AvaliacaoResponseDTO>> listarAvaliacoesPorPersonal(@PathVariable Integer idPersonal) {
+        return avaliacaoService.listarAvaliacoesPorPersonal(idPersonal);
+    }
+
+    @PostMapping()
+    public ResponseEntity<AvaliacaoResponseDTO> criarAvaliacao(@RequestBody AvaliacaoRequestDTO avaliacaoRequestDTO) {
+        // Implementar a lógica para criar uma nova avaliação
+        return avaliacaoService.cadastrarAvaliacao(avaliacaoRequestDTO);
+    }
+}
