31c2a79d4f7df9feddbf6a638618b72a6c013ee9
Gustavo Kohatsu <gustavo.kohatsu@sptech.school>
2025-10-05 00:30:01 -0300

    fix: adicionando variaveis nos properties dev e prod

--- src/main/java/tech/vitalis/caringu/config/SecurityConfiguracao.java
+++ src/main/java/tech/vitalis/caringu/config/SecurityConfiguracao.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -134,7 +133,7 @@ public class SecurityConfiguracao {
 
         configuracao.setAllowedHeaders(List.of("*"));
         configuracao.setExposedHeaders(List.of(HttpHeaders.CONTENT_DISPOSITION, "Set-Cookie"));
-        
+
         // Configurações para suportar cookies
         configuracao.setAllowCredentials(true);
 
