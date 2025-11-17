8d3a30f4dc1961bf3243083df553315aa2d0354a
biasouza21 <bianca.bs2211@gmail.com>
2025-11-17 12:19:48 -0300

    endpoint post feedback

--- src/main/java/tech/vitalis/caringu/dtos/Feedback/CriacaoFeedbackDto.java
+++ src/main/java/tech/vitalis/caringu/dtos/Feedback/CriacaoFeedbackDto.java
@@ -1,4 +1,13 @@
 package tech.vitalis.caringu.dtos.Feedback;
 
-public class CriacaoFeedbackDto {
-}
+import tech.vitalis.caringu.enums.Feedback.TipoAutorEnum;
+
+import java.time.LocalDateTime;
+
+public record CriacaoFeedbackDto (
+        Integer autorId,
+        Integer aulaId,
+        TipoAutorEnum autorTipo,
+        String descricao,
+        LocalDateTime dataCriacao
+){}
